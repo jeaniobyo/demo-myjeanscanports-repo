cbeeb9beec5fca7d1a0d71325f340b456874c6f9
Jean Iobyo <jean_iobyo@stu.indianhills.edu>
2022-11-01 13:23:01 -0500

    initial commit

--- clientsocket.py
+++ clientsocket.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
 import socket 
-from datetime import date
 
-Today = date.today()
-print(Today)
-c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 80
-c.connect((host, port))
-c.recv(80)
-c.close 
+port = 443
+s.connect(('', port))
+print(s.recv(80))
+s.close()
--- port_scanner.py
+++ port_scanner.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python 
 
 import socket
-from datetime import date 
-import sys
 
-Today = date.today()
-print(Today)
-
-j=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 80
-j.bind ((host, port))
+port = 443
+s.bind (("", port))
 print("connections completed successfully")
-j.listen(3)
+s.listen(3)
 print("listening for client connections")
 while True:
-    c, addr = j.accept()
+    c, addr = s.accept()
     print("connected successfully with", addr)
     c.send("Welcome to Jean Server!!!!!!")
-    c.close
+    c.close()
