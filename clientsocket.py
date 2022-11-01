1b29a7a640e93426aa55e75ea77c94e62429bd64
Jean Iobyo <jean_iobyo@stu.indianhills.edu>
2022-11-01 14:11:45 -0500

    initial commit

--- clientsocket.py
+++ clientsocket.py
@@ -4,7 +4,9 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 443
-s.connect(('', port))
-print(s.recv(80))
+port = 1245
+s.connect((host,port))
+message = "This is hi from client"
+s.send(message.encode("utf-8"))
+
 s.close()
--- port_scanner.py
+++ port_scanner.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python 
 
+from email.headerregistry import Address
 import socket
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 443
-s.bind (("", port))
-print("connections completed successfully")
-s.listen(3)
-print("listening for client connections")
-while True:
-    c, addr = s.accept()
-    print("connected successfully with", addr)
-    c.send("Welcome to Jean Server!!!!!!")
-    c.close()
+port = 1245
+s.bind ((host,port))
+s.listen(5)
+socketclient,address=s.accept()
+print("connected recieved from other terminal",address)
+message=socketclient.recv(1024)
+message=message.decode("utf-8")
+print(message)
+s.close()
