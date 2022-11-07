b07a5d9ab52d01dd1ad9dcfe3c216ee843ca2178
unknown <Administrator@LAB02.cybersec.local>
2022-11-07 14:35:18 -0600

    initial

--- portscanner.py
+++ portscanner.py
@@ -12,14 +12,18 @@ print(F.renderText("PORT SCANNER"))
 Today = datetime.now()
 print("Scanning started at:", Today)
 
-print("-" * 50)
-Host = socket.gethostbyname_ex()
-for port in range(1,655335):
-    s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = s.connect_ex((Host, port))
-    if result == 0:
-        print("Port {} is open".format(port))
-    s.close()
+print("-" * 70)
+host = socket.gethostbyaddr()
+def new_func(host):
+        for port in range(1,655335):
+            s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = s.connect((host, port))
+        if result == 0:
+            print("Port is open")
+        else:
+            print()
+        s.close("port is closed")
 
-print(sys.copyright)
-sys.exit()
+new_func(host)
+
+
