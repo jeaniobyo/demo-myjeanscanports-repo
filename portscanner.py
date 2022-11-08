d08968fde56456e17754e330d6347806a9a03ce2
unknown <Administrator@LAB02.cybersec.local>
2022-11-08 12:51:02 -0600

    initial

--- portscanner.py
+++ portscanner.py
@@ -19,7 +19,7 @@ print("-" * 70) #This code will show the bottom dots border line between scannin
 
 for port in range(80,443): #This code will show the ranges of the ports, will be scanning.
     n = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #This code show I be scanning IPV4 and with the TCP connections.
-    result = n.connect_ex(("10.10.90.16", port)) #This code focus on which host IP address will be scanning plus the port variable.
+    result = n.connect_ex(("10.10.64.115", port)) #This code focus on which host IP address will be scanning plus the port variable.
 
     if result == 0:    
         print("port: " + str(port) + " is open") # I got this code from (Herbertech on youtube under 'build a basic port scanner in python') This code will display port number plus statement saying the port is open.
